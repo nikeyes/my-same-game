6d92c611a5b5c985eba4ac1376210ef42fe904b0
FUSION\jorge.castro <jorge.castro@habitaclia.com>
2020-04-05 18:21:39 +0200

    Window Resize

--- game.js
+++ game.js
@@ -22,9 +22,26 @@ window.onload = function() {
     }
     game = new Phaser.Game(gameConfig);
     window.focus()
-    //resize();
-    //window.addEventListener("resize", resize, false);
+    resize();
+    window.addEventListener("resize", resize, false);
 }
+
+function resize() {
+    var canvas = document.querySelector("canvas");
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    var windowRatio = windowWidth / windowHeight;
+    var gameRatio = game.config.width / game.config.height;
+    if(windowRatio < gameRatio){
+        canvas.style.width = windowWidth + "px";
+        canvas.style.height = (windowWidth / gameRatio) + "px";
+    }
+    else{
+        canvas.style.width = (windowHeight * gameRatio) + "px";
+        canvas.style.height = windowHeight + "px";
+    }
+}
+
 
 class gemGame extends Phaser.Scene{
     constructor(){
