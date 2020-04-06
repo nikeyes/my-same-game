2d634e79dd1c80e6c9b2de6e8b83554e5547e71f
FUSION\jorge.castro <jorge.castro@habitaclia.com>
2020-04-07 01:01:29 +0200

    v0.2

--- game.js
+++ game.js
@@ -4,7 +4,7 @@
  * https://www.emanueleferonato.com/2016/10/05/html5-samegame-engine-powered-by-phaser/
  * https://www.emanueleferonato.com/2019/01/14/complete-html5-samegame-game-for-you-to-play-and-download-featuring-no-more-moves-check-powered-by-phaser-3-and-pure-javascript-samegame-class/
 */
-const MIN_GEMS_TO_DESTROY = 1;
+const MIN_GEMS_TO_DESTROY = 2;
 let gameOptions = {
     gemSize: 80, 
     boardSize: {
